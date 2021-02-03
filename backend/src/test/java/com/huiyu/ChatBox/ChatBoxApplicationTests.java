1771932267725741fb954597bd9bd863455f0682
Yu Sun <derek.y.sun@gmail.com>
2021-02-03 12:35:00 -0800

    backend init commit

---
+++ backend/src/test/java/com/huiyu/ChatBox/ChatBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huiyu.ChatBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
