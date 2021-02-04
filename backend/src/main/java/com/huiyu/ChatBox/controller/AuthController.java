c0d55daba49a2318371899421b83747c470907da
Yu Sun <derek.y.sun@gmail.com>
2021-02-03 18:04:05 -0800

    oauth

---
+++ backend/src/main/java/com/huiyu/ChatBox/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.huiyu.ChatBox.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class AuthController {
+
+    //    @RequestMapping("/")
+//    public String hello (){
+//        return "hello";
+//    }
+}
