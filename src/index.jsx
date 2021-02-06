8b1c387dd696234de4a2e3a71c84da3627e8aff0
yiyi1026 <vivian.li.uibe@gmail.com>
2021-02-05 19:06:16 -0800

    frontend test

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import App from './app'
+
 const rootEl = document.getElementById('root');
-ReactDOM.render(<h1>BBB</h1>, rootEl);
+ReactDOM.render(<h1>App
+  <App/>
+</h1>, rootEl);
